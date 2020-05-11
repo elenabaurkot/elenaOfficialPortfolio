c4806081a056f9f17ca5f83d4fc390e811c95496
elenabaurkot <baurkot@Baurkots-iMac.fios-router.home>
2020-05-11 15:18:27 -0400

    project section styling complete

--- app.js
+++ app.js
@@ -1,22 +1,18 @@
+// RESUME MODAL
 // Get the modal
 var modal = document.getElementById("myModal");
-
 // Get the button that opens the modal
 var btn = document.getElementById("myBtn");
-
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
-
 // When the user clicks on the button, open the modal
 btn.onclick = function() {
   modal.style.display = "block";
 }
-
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   modal.style.display = "none";
 }
-
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
@@ -25,7 +21,95 @@ window.onclick = function(event) {
 }
 
 
+// PROJECT MODALS
+// YellowChat Modal
+var projModal = document.getElementsByClassName("proModal")[0];
+var proBtn = document.getElementsByClassName("proBtn")[0];
+// When the user clicks on the button, open the modal
+proBtn.onclick = function() {
+  projModal.style.display = "block";
+}
+// Get the <span> element that closes the modal
+var closeOut = document.getElementsByClassName("proj-close")[0];
+// When the user clicks on <span> (x), close the modal
+closeOut.onclick = function() {
+  projModal.style.display = "none";
+}
 
+// FlipCards Modal
+var projModal1 = document.getElementsByClassName("proModal")[1];
+var proBtn1 = document.getElementsByClassName("proBtn")[1];
+// When the user clicks on the button, open the modal
+proBtn1.onclick = function() {
+  projModal1.style.display = "block";
+}
+// Get the <span> element that closes the modal
+var closeOut1 = document.getElementsByClassName("proj-close")[1];
+// When the user clicks on <span> (x), close the modal
+closeOut1.onclick = function() {
+  projModal1.style.display = "none";
+}
+
+// Weather Dashboard Modal
+var projModal2 = document.getElementsByClassName("proModal")[2];
+var proBtn2 = document.getElementsByClassName("proBtn")[2];
+// When the user clicks on the button, open the modal
+proBtn2.onclick = function() {
+  projModal2.style.display = "block";
+}
+// Get the <span> element that closes the modal
+var closeOut2 = document.getElementsByClassName("proj-close")[2];
+// When the user clicks on <span> (x), close the modal
+closeOut2.onclick = function() {
+  projModal2.style.display = "none";
+}
+
+// Day Planner Modal
+var projModal3 = document.getElementsByClassName("proModal")[3];
+var proBtn3 = document.getElementsByClassName("proBtn")[3];
+// When the user clicks on the button, open the modal
+proBtn3.onclick = function() {
+  projModal3.style.display = "block";
+}
+// Get the <span> element that closes the modal
+var closeOut3 = document.getElementsByClassName("proj-close")[3];
+// When the user clicks on <span> (x), close the modal
+closeOut3.onclick = function() {
+  projModal3.style.display = "none";
+}
+
+// Password Generator Modal
+var projModal4 = document.getElementsByClassName("proModal")[4];
+var proBtn4 = document.getElementsByClassName("proBtn")[4];
+// When the user clicks on the button, open the modal
+proBtn4.onclick = function() {
+  projModal4.style.display = "block";
+}
+// Get the <span> element that closes the modal
+var closeOut4 = document.getElementsByClassName("proj-close")[4];
+// When the user clicks on <span> (x), close the modal
+closeOut4.onclick = function() {
+  projModal4.style.display = "none";
+}
+
+// Code Quiz Modal
+var projModal5 = document.getElementsByClassName("proModal")[5];
+var proBtn5 = document.getElementsByClassName("proBtn")[5];
+// When the user clicks on the button, open the modal
+proBtn5.onclick = function() {
+  projModal5.style.display = "block";
+}
+// Get the <span> element that closes the modal
+var closeOut5 = document.getElementsByClassName("proj-close")[5];
+// When the user clicks on <span> (x), close the modal
+closeOut5.onclick = function() {
+  projModal5.style.display = "none";
+}
+
+
+
+
+// NAVIGATION BAR ANIMATION
 const navSlide = () => {
   const burger = document.querySelector('.burger');
   const nav = document.querySelector('.nav-links');
